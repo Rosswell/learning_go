f353674164de54432c95fcd7935515f1d3375872
Ross Blanchard <ross.blanchard@helix.com>
2018-02-08 11:07:27 -0800

    comments amended to functionality

--- values.go
+++ values.go
@@ -4,11 +4,14 @@ import "fmt"
 
 func main() {
 
+	// concatting strings
 	fmt.Println("go" + "lang")
 
+	// ints and floats
 	fmt.Println("1+1=", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
 
+	// bool comparison
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
--- variables.go
+++ variables.go
@@ -4,18 +4,23 @@ import "fmt"
 
 func main() {
 
+	// type is inferred
 	var a = "initial"
 	fmt.Println(a)
 
+	// var allows multiple declarations in the same statement
 	var b, c int = 1, 2
 	fmt.Println(b, c)
 
+	// type is still inferred
 	var d = true
 	fmt.Println(d)
 
+	// variables without an initial value are zero-valued, which is 0 for ints
 	var e int
 	fmt.Println(e)
 
+	// := declares the variable, infers the type, and assigns the value. Equivalent to f string = "short"
 	f := "short"
 	fmt.Println(f)
 }
